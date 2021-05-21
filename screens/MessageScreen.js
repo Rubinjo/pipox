1da5815aa468f1aca8f79cecb8355b2502a4fa32
Ruben <ruben.lucas1997@gmail.com>
2021-05-21 21:52:22 +0200

    💄 Reaction input

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
-import { View, FlatList, Text, StyleSheet } from "react-native";
+import {
+  View,
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { useSelector } from "react-redux";
+import { Entypo } from "@expo/vector-icons";
 
 import MessageCard from "../components/MessageCard";
 import ReactionCard from "../components/ReactionCard";
@@ -9,6 +17,7 @@ import Config from "../components/Config";
 
 const MessageScreen = (props) => {
   const [refresh, setRefresh] = useState(false);
+  const [postReaction, setPostReaction] = useState("");
 
   const handleRefresh = () => {
     setRefresh(true);
@@ -28,11 +37,42 @@ const MessageScreen = (props) => {
         data={message.reactions}
         renderItem={(itemData) => <ReactionCard reaction={itemData.item} />}
         keyExtractor={(item) => item.id.toString()}
-        ListEmptyComponent={<Text>KLOPT NIET</Text>}
+        ListEmptyComponent={
+          <View
+            style={{
+              alignItems: "center",
+              marginTop: Config.deviceHeight * 0.02,
+            }}
+          >
+            <Text>This post has no reactions yet.</Text>
+          </View>
+        }
         // contentContainerStyle={styles.listContainer}
         refreshing={refresh}
         onRefresh={() => this.handleRefresh()} // Not yet working
       />
+      <View style={styles.counter}>
+        <Text style={styles.counterText}>250/{postReaction.length}</Text>
+      </View>
+      <View style={styles.searchBar}>
+        <TextInput
+          style={styles.searchInput}
+          onChangeText={(text) => setPostReaction(text)}
+          value={postReaction}
+          keyboardAppearance="dark"
+          placeholder="Add your reaction..."
+          placeholderTextColor={COLORS.Foreground}
+          multiline={true}
+          returnKeyType="done"
+        />
+        <TouchableOpacity>
+          <Entypo
+            name="arrow-with-circle-right"
+            size={22 + Config.deviceHeight * 0.006}
+            color={COLORS.PrimaryColorOn}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -44,6 +84,29 @@ const styles = StyleSheet.create({
     alignContent: "center",
     backgroundColor: COLORS.Background,
     paddingTop: Config.deviceHeight * 0.003,
+  },
+  searchBar: {
+    width: "95%",
+    alignSelf: "center",
+    flexDirection: "row",
+    paddingVertical: Config.deviceHeight * 0.003,
+    marginTop: Config.deviceHeight * 0.01,
+    marginBottom: Config.deviceHeight * 0.02,
+    borderRadius: 8,
+    backgroundColor: COLORS.white,
+    maxHeight: Config.deviceHeight * 0.182,
+  },
+  searchInput: {
+    marginLeft: Config.deviceWidth * 0.025,
+    width: "89%",
+  },
+  counter: {
+    alignItems: "flex-end",
+    paddingRight: Config.deviceWidth * 0.06,
+  },
+  counterText: {
+    color: COLORS.grey,
+    fontFamily: "segoe-ui-regular",
   },
 });
 
